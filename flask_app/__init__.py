b94efdd76b03523cfea7635249ce717d3b6620fb
devolo13 <devolo13@gmail.com>
2023-04-27 16:15:43 -0500

    hid secret key

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from secret_key import secret_key
 
 app = Flask(__name__)
-app.secret_key = 'Harry Potter was mid'
+# in secret_key.py is secret_key = 'some random string thing'
+app.secret_key = secret_key
 
 DATABASE = 'social_media_schema'
